eedb5d3ab74fcf36fe4f40bb180dd1c517cd4332
Gustavo Borba <gustavo.borba2001@gmail.com>
2023-06-05 13:24:26 -0300

    Base do jogo e relação com robo e mapa

--- FinalGame/JewelCollector.cs
+++ FinalGame/JewelCollector.cs
@@ -1,27 +1,73 @@
+using System;
+using Jwl;
+using Mp;
+using Obst;
+using Rbt;
+
+namespace Jc
+{
 public class JewelCollector {
 
-  public static void Main() {
-  
-      bool running = true;
-  
-      do {
-  
-          Console.WriteLine("Enter the command: ");
-          string command = Console.ReadLine();
-  
-          if (command.Equals("quit")) {
-              running = false;
-          } else if (command.Equals("w")) {
-              
-          } else if (command.Equals("a")) {
-              
-          } else if (command.Equals("s")) {
+    public static void Main() {
+    
+        bool running = true;
+
+        Mp.Map.MapInfo mapinfo = new Mp.Map.MapInfo();
+        Robot rbt = new Robot();
+
+        string[,] mapa = mapinfo.Cell;
+        void PrintMap()
+        {
+                        // Printagem do mapa
+            for (int i = 0; i < mapa.GetLength(0); i++)
+            {
+                for (int j = 0; j < mapa.GetLength(1); j++)
+                {
+                    Console.Write(mapa[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }    
+
+        do {
+            PrintMap();
+            Console.WriteLine("Enter the command: ");
+
+            string? command = Console.ReadLine();
+
+            if (command.Equals("quit")) {
+                running = false;
+                
+            } else if (command.Equals("w")) {
+                
+                rbt.Up();
+                
+            } else if (command.Equals("a")) {
+
+                rbt.Left();
+                
+            } else if (command.Equals("s")) {
+
+                rbt.Down();
+
             
-          } else if (command.Equals("d")) {
-          
-          } else if (command.Equals("g")) {
-              
-          }
-      } while (running);
-  }
-}
+                
+            } else if (command.Equals("d")) {
+                
+                rbt.Right();
+
+            
+            } else if (command.Equals("g")) {
+        
+        
+
+            } else {
+
+                Console.WriteLine("Por favor, digite algum comando válido");
+            
+            }
+        } while (running);
+    }
+    }
+}    
+    
